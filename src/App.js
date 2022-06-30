3ccf4bd2dbdd18e8b98e617a695f0654a98dec53
marccorpus <marccorpus.dev@gmail.com>
2022-07-01 01:04:30 +0800

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import Categories from "./components/categories/categories";
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./components/navigation/Navigation";
+import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Categories categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
